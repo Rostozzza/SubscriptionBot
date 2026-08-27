5adad925fc7157cef28ff82f0285a9854ce3851b
Rostozavr <gfiyskgs@gmail.com>
2026-08-27 23:31:13 +0300

    hotfix

--- caban_on_awake.py
+++ caban_on_awake.py
@@ -14,6 +14,9 @@ def add_cabans_from_file(session_factory: sessionmaker, path: str):
                 with open(path, "r") as file:
                     for line in file:
                         tg_id = int(line)
+                        if db.is_caban(tg_id):
+                            logger.info(f"Кабан {tg_id} уже существует")
+                            continue
                         db.add_caban(tg_id, datetime.now(), 0)
                         logger.info(f"Добавлен кабан со старта: {tg_id}")
     except Exception as e:
